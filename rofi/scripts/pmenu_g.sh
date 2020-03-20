4c7ab70c8d321baa66f10fbf78f02dfad01e56a1
Kishan <kishan@kishan.sh>
2020-03-20 12:39:59 +0530

    updating theme

--- rofi/scripts/pmenu_g.sh
+++ rofi/scripts/pmenu_g.sh
@@ -2,7 +2,7 @@
 
 ## Created By Aditya Shakya
 
-MENU="$(rofi -sep "|" -dmenu -i -p '' -location 3 -yoffset 40 -xoffset -10  -margin 0 -hide-scrollbar -line-padding 0 -padding 0 -width 7 -lines 4 -font "Iosevka Nerd Font Mono 15" <<< "Lock|Logout|Reboot|Shutdown")"
+MENU="$(rofi -sep "|" -dmenu -i -p '' -location 3 -yoffset 55 -xoffset -15  -margin 0 -hide-scrollbar -line-padding 0 -padding 0 -width 6 -lines 4 -font "Dank Mono 13" <<< "Lock|Logout|Reboot|Shutdown")"
             case "$MENU" in
                 *Lock) sh ~/.config/xlock/i3lock.sh;;
                 *Logout) bspc quit;;
