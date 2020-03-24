a73cbca414981f34760090db7d81fe0fea1f11df
Kishan <kishan@kishan.sh>
2020-03-25 00:07:07 +0530

    UPDATE

--- bspwm/xrand.sh
+++ bspwm/xrand.sh
@@ -4,5 +4,5 @@
 # want to apply.
 my_external_monitor=$(xrandr --query | grep 'DP-0')
 if [[ $my_external_monitor = *connected* ]]; then
-    xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-3 --off
+    xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --rate 120 --pos 2560x0 --rotate normal --output DP-3 --off
 fi
