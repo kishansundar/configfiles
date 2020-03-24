f1a744a1e57d4e620c0e1f8f98ce56db79dcf2df
Kishan <kishan@kishan.sh>
2020-03-24 22:19:11 +0530

    update dual monitor setup

---
+++ bspwm/xrand.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# set up the two monitors for bspwm
+# NOTE This is a simplistic approach because I already know the settings I
+# want to apply.
+my_external_monitor=$(xrandr --query | grep 'DP-0')
+if [[ $my_external_monitor = *connected* ]]; then
+    xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-3 --off
+fi
