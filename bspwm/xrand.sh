7a07a760d71b534044a329702c5540593ea87fdd
Kishan <kishan@kishan.sh>
2020-03-26 04:09:51 +0530

    update

--- bspwm/xrand.sh
+++ bspwm/xrand.sh
@@ -2,7 +2,9 @@
 # set up the two monitors for bspwm
 # NOTE This is a simplistic approach because I already know the settings I
 # want to apply.
+xrandr --output DP-0 --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DP-2 --primary --mode 3440x1440 --rate 120 --pos 2560x0 --rotate normal
+
 my_external_monitor=$(xrandr --query | grep 'DP-0')
 if [[ $my_external_monitor = *connected* ]]; then
-    xrandr --output DP-0 --primary --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DP-2  --mode 3440x1440 --rate 120 --pos 2560x0 --rotate normal
+    xrandr --output DP-0 --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DP-2 --primary --mode 3440x1440 --rate 120 --pos 2560x0 --rotate normal
 fi
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 kill $(pgrep polybay)
 
-polybar DP0 -c ~/.config/polybar/config.ini &
+   polybar DP2 -c ~/.config/polybar/config.ini &
 
 # Launch bar1 and bar2
 my_external_monitor=$(xrandr --query | grep 'DP-0')
 if [[ $my_external_monitor = *connected* ]]; then
-   polybar DP2 -c ~/.config/polybar/config.ini &
+   polybar DP0 -c ~/.config/polybar/config.ini &
 fi
 
 
